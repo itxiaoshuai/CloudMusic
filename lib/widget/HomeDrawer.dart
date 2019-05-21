dea508ee044bcea70faf0c374915144dae33f41b
罗帅 <luoshuai@boyue.com>
2019-05-21 18:31:19 +0800

    侧边栏头部 UI展示

--- lib/widget/HomeDrawer.dart
+++ lib/widget/HomeDrawer.dart
@@ -12,7 +12,9 @@ class _HomeDrawerState extends State {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
+        padding: EdgeInsets.zero,
         children: <Widget>[
+          Head(),
           DrawerListItemHorizontal(),
           DrawerListItem(image: "images/ticket.png", text: "演出"),
           DrawerListItem(image: "images/shopping_cart.png", text: "商城"),
@@ -69,5 +71,57 @@ class ListItem extends StatelessWidget {
                 )))
       ],
     );
+  }
+}
+
+class Head extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    _log() {
+      print("点击了边线按钮");
+    }
+
+    return Container(
+        child: Container(
+      padding: EdgeInsets.only(top: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Text(
+              '登录网易云音乐',
+              style: TextStyle(color: Colors.black, fontSize: 12),
+            )
+          ]),
+          Container(margin: EdgeInsets.all(2)), //文字之间添加 margin
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Text(
+              '手机电脑多端同步,尽享海量高品质音乐',
+              style: TextStyle(color: Colors.black, fontSize: 12),
+            )
+          ]),
+          Container(
+            margin: EdgeInsets.only(top: 14),
+            child: OutlineButton(
+              highlightColor: Colors.black,
+              onPressed: _log,
+
+              padding: EdgeInsets.only(left: 45, right: 45),
+              child: Text(
+                "立即登录",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+              shape: StadiumBorder(
+                side: BorderSide(width: 16.0, color: Colors.lightBlue.shade900),
+              ),
+              //一个边框，适合体育场形状的边框（两端有半圆的框），适用于它所应用的小部件的矩形
+//              borderSide: BorderSide(color: Colors.grey), //默认边框颜色
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
